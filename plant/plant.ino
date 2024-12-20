6474df3a20d57c5e681f0df3a67d5008b5d6e78a
문기준 <moongijun@gnu.ac.kr>
2024-12-20 16:21:52 +0900

    Update plant.ino

--- plant/plant.ino
+++ plant/plant.ino
@@ -1,32 +1,28 @@
 #include <MQUnifiedsensor.h>
 #include <Adafruit_NeoPixel.h>
-#include <avr/power.h>
-#include <Wire.h>
-#include <RTClib.h>
 #include <DHT.h>
 
 #define DHTPIN 5
 #define ledPin 7
-#define relayPin 12
-#define RTCSDA A4
-#define RTCSCL A5
-#define RTCSQW 13
+#define motorRelay 12
 #define soilHumiditySensor A0
 #define lightSensor A1
 #define waterLevelSensor A2
-#define PHSensor A2
+#define PHSensor A4
 #define MQ135Pin A3           
 
 #define placa "Arduino UNO"
 #define RatioMQ135CleanAir 3.6  // RS/R0 = 3.6 ppm
 
+#define R_DIV 4660.0
+
 #define Vref 4.95
 int i=0;
+int count = 0;
 
 Adafruit_NeoPixel pixels(20, ledPin, NEO_GRB + NEO_KHZ800);
 MQUnifiedsensor MQ135(placa, 5, 10, MQ135Pin, "MQ-135");  // 센서 선언
 DHT dht(DHTPIN, DHT11);
-RTC_DS1307 rtc;
 
 struct DHTData {
   float humidity;
@@ -36,13 +32,10 @@ struct DHTData {
 void waterPump();//
 void light(int R, int G, int B);///
 int readSoilHumidity();/// 수분이 많을수록 0에 가까움
-int readLight();///
+float readLight();///
 int readWaterlevel();///
 float readPH();//
 float readAir();///
-void readTime();///
-void relayOn();///
-void relayOff();///
 DHTData readDHT();///
 
 
@@ -50,18 +43,18 @@ DHTData readDHT();///
 void setup() {
   Serial.begin(9600);           // 시리얼 포트 초기화
 
-  pinMode(motorIN3, OUTPUT);
-  pinMode(motorIN4, OUTPUT);
-  pinMode(motorSpeedB, OUTPUT);
   pinMode(ledPin, OUTPUT);
-  pinMode(relayPin, OUTPUT);
+  pinMode(motorRelay, OUTPUT);
+
+  pinMode(DHTPIN, INPUT);
+
   pinMode(soilHumiditySensor, INPUT);
   pinMode(lightSensor, INPUT);
   pinMode(waterLevelSensor, INPUT);
   pinMode(PHSensor, INPUT);
 
   dht.begin(); // DHT 센서 초기화
-
+  digitalWrite(motorRelay, HIGH);
 // 이산화탄소 
 
   MQ135.setRegressionMethod(1);  // _PPM =  a*ratio^b (PPM 농도와 상수 값을 계산하기 위한 수학 모델 설정)
@@ -90,31 +83,11 @@ void setup() {
     pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
   
   Serial.println("setup END");
-//RTC
-  if (!rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    while (1);
-  }
-  if (!rtc.isrunning()) {
-    Serial.println("RTC is NOT running, let's set the time!");
-    // 다음 줄은 컴파일된 시점의 날짜 및 시간으로 설정합니다.
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-}
 
 }
 
 void loop() {////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  // Serial.println(readAir());
-  // Serial.println(readLight());
-  // Serial.println(readWaterLevel());
-  // Serial.println(readSoilHumidity());
-  // light(255, 255, 255);
-  // readTime();
-  // DHTData dhtData = readDHT();
-  // Serial.println(dhtData.humidity); // %
-  // Serial.println(dhtData.temperature); // *C
-
-  int light = readLight();
+  float currentLight = readLight();
   delayMicroseconds(10);
   int water = readWaterLevel();
   delayMicroseconds(10);
@@ -122,31 +95,52 @@ void loop() {///////////////////////////////////////////////////////////////////
   delayMicroseconds(10);
   float CO2 = readAir();
   delayMicroseconds(10);
+  float PH = readPH();
+  delayMicroseconds(10);
   DHTData dhtData = readDHT();
   delayMicroseconds(10);
+  
+  
 
-  if (light<500) {
+  Serial.print(currentLight);
+  Serial.print(",");
+  Serial.print(water);
+  Serial.print(",");
+  Serial.print(soilHumidity);
+  Serial.print(",");
+  Serial.print(CO2);
+  Serial.print(",");
+  Serial.print(PH);
+  Serial.print(",");
+  Serial.print(dhtData.temperature);
+  Serial.print(",");
+  Serial.println(dhtData.humidity);
+
+
+  if (currentLight<200) {
     light(255,255,255);
   } else {
     light(0,0,0);
   }
-  if (soilHumidity < 400) {
-    waterPump();
+  if (soilHumidity  > 400) {
+    count++;
+    if (count > 15) {
+      waterPump();
+      count = 0;
+    }
+  } else {
+    count = 0;
   }
 
 
   delay(1000);
 }
 
-void waterPump() { //////////////// 작동안함
-  Serial.println("pump");
-  digitalWrite(motorIN3, HIGH);
-  digitalWrite(motorIN4, LOW);
-  analogWrite(motorSpeedB, 255);
-  
+void waterPump() { 
+  digitalWrite(motorRelay, LOW);
   delay(1000);
-
-  analogWrite(motorSpeedB, 0);
+  
+  digitalWrite(motorRelay, HIGH);
 }
 
 float readPH() {
@@ -155,7 +149,6 @@ float readPH() {
   long sensorSum;
   unsigned long int avgValue;
   int buf[10];                
-  relayOff();
   for(int i=0;i<10;i++)       
   { 
     buf[i]=analogRead(PHSensor);
@@ -189,13 +182,7 @@ float readAir() {
     return CO2+400;
 }
 
-void relayOn() {
-  digitalWrite(relayPin, HIGH);
-}
 
-void relayOff() {
-  digitalWrite(relayPin, LOW);
-}
 
 void light(int R , int G, int B) {
   pixels.clear(); // Set all pixel colors to 'off'
@@ -208,13 +195,15 @@ void light(int R , int G, int B) {
   pixels.show();   // Send the updated pixel colors to the hardware.
 }
 
-int readLight() {
+float readLight() {
   int light = analogRead(lightSensor);
-  return light;
+  float voltage = light * (5.0 / 1023.0);
+  float resistance = R_DIV * ((5.0 / voltage) - 1.0);
+  float lux = 500000.0 / resistance;
+  return lux;
 }
 
 int readWaterLevel() {
-  relayOn();
   delay(10);
   return analogRead(waterLevelSensor);
 }
@@ -222,23 +211,6 @@ int readWaterLevel() {
 int readSoilHumidity() {
   return analogRead(soilHumiditySensor);
 }
-void readTime() {
-  DateTime now = rtc.now();
-
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(" ");
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.println();
-}
-
 
 DHTData readDHT() {
   DHTData data;
@@ -246,11 +218,6 @@ DHTData readDHT() {
   data.temperature = dht.readTemperature(); // 섭씨 온도 읽기
   return data;
 }
-
-
-
-
-
 
 
 
